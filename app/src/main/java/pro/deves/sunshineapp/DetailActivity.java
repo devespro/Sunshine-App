7a744d39f486da852263cc2bf7d8944948576455
Esien Novruzov <esien.novruzov@gmail.com>
2017-02-19 12:22:56 +0100

    added settings activity

---
+++ app/src/main/java/pro/deves/sunshineapp/DetailActivity.java
@@ -0,0 +1,22 @@
+package pro.deves.sunshineapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by deves on 18/02/17.
+ */
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.detail_container, new DetailFragment())
+                    .commit();
+        }
+    }
+
+}
---
+++ app/src/main/java/pro/deves/sunshineapp/DetailFragment.java
@@ -0,0 +1,55 @@
+package pro.deves.sunshineapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by deves on 18/02/17.
+ */
+public class DetailFragment extends Fragment {
+
+    public static final String DETAIL_EXTRA_TEXT = "detail_info";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.item_detail_textview);
+        Intent intent = getActivity().getIntent();
+        if (intent != null && intent.hasExtra(DETAIL_EXTRA_TEXT)) {
+            String selectedItemData = intent.getExtras().getString(DETAIL_EXTRA_TEXT);
+            textView.setText(selectedItemData);
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+        inflater.inflate(R.menu.main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
