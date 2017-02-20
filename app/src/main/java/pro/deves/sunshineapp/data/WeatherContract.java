a60719e2ddcd4781893a44f44bfce5d0ffce2140
Esien Novruzov <esien.novruzov@gmail.com>
2017-02-20 16:53:52 +0100

    added weather content provider

--- app/src/main/java/pro/deves/sunshineapp/data/WeatherContract.java
+++ app/src/main/java/pro/deves/sunshineapp/data/WeatherContract.java
@@ -1,5 +1,8 @@
 package pro.deves.sunshineapp.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 import android.text.format.Time;
 
@@ -7,7 +10,12 @@ import android.text.format.Time;
  * Defines table and column names for the weather database.
  */
 public class WeatherContract {
+    public static final String CONTENT_AUTHORITY = "pro.deves.sunshineapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
+    // Possible paths (appended to base content URI for possible URI's)
+    public static final String PATH_WEATHER = "weather";
+    public static final String PATH_LOCATION = "location";
     // To make it easy to query for the exact date, we normalize all dates that go into
     // the database to the start of the the Julian day at UTC.
     public static long normalizeDate(long startDate){
@@ -18,15 +26,32 @@ public class WeatherContract {
     }
 
     public static final class LocationEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_LOCATION;
         public static final String TABLE_NAME = "location";
 
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
         public static final String COLUMN_CITY = "city";
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static Uri buildLocationUriById(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
+    /**
+     * Inner class that defines the table contents of the weather table
+     */
     public static final class WeatherEntry implements BaseColumns {
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_WEATHER;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + PATH_WEATHER;
         public static final String TABLE_NAME = "weather";
         public static final String COLUMN_LOCATION_KEY  = "location_id";
         public static final String COLUMN_DATE  = "date";
@@ -39,5 +64,43 @@ public class WeatherContract {
         public static final String COLUMN_PRESSURE = "pressure";
         public static final String COLUMN_WIND_SPEED = "wind";
         public static final String COLUMN_DEGREES = "degrees";
+
+        public static Uri buildWeatherUriById(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+        public static Uri buildWeatherLocation(String locationSetting){
+            return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
+        }
+
+        public static Uri buildWeatherLocationWithStartDate(String locationSetting, long startDate){
+            long normalizedDate = normalizeDate(startDate);
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendQueryParameter(COLUMN_DATE, Long.toString(normalizedDate))
+                    .build();
+        }
+
+        public static Uri buildWeatherLocationWithDate(String locationSetting, long date){
+            long normalizedDate = normalizeDate(date);
+            return CONTENT_URI.buildUpon().appendPath(locationSetting)
+                    .appendPath(Long.toString(normalizedDate)).build();
+        }
+
+        public static String getLocationSettingFromUri(Uri uri){
+            return uri.getPathSegments().get(1);
+        }
+
+        public static long getDateFromUri(Uri uri){
+            return Long.parseLong(uri.getPathSegments().get(2));
+        }
+
+        public static long getStartDateFromUri(Uri uri){
+            String dateString = uri.getQueryParameter(COLUMN_DATE);
+            if (dateString != null && dateString.length() > 0){
+                return Long.parseLong(dateString);
+            } else {
+                return 0;
+            }
+        }
     }
 }
